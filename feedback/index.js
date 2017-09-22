119f3859f4bbe5509a95228d57f33c717e645353
huhaitao <huhaitao@100tal.com>
2017-09-22 14:51:08 +0800

    add ready event

--- feedback/index.js
+++ feedback/index.js
@@ -1,1 +1,5 @@
-alert(JSON.stringify(WeixinJSBridge))
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+  alert(JSON.stringify(WeixinJSBridge))
+  // 通过下面这个API隐藏右上角按钮
+  WeixinJSBridge.call('hideOptionMenu');
+});
