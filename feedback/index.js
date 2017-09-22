2eefe40f06deb14d242428db381988af227ae318
huhaitao <huhaitao@100tal.com>
2017-09-22 15:01:30 +0800

    alert agent

--- feedback/index.js
+++ feedback/index.js
@@ -1,5 +1,1 @@
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-  alert(JSON.stringify(WeixinJSBridge))
-  // 通过下面这个API隐藏右上角按钮
-  // WeixinJSBridge.call('hideOptionMenu');
-});
+alert(navigator.userAgent)
